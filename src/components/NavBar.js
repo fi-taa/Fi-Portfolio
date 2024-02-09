65b2f510d5af320427c26605f3b20c2c64378877
Fita-Wegene <fitaawd@gmail.com>
2024-02-09 15:19:34 +0300

    update the nav bar look

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,54 +22,66 @@ function NavBar() {
   };
 
   return (
-    <div className="m-0 p-0 h-[12vh]">
+    <div className="m-0 p-0 h-[12vh] bg-black">
       <nav className="flex justify-center items-center relative">
         <a href="/">
-          <Image src="/logo-icon.svg" width={100} height={100} />
+          <div className="text-white m-5 font-light text-3xl overline">Fitaa</div>
         </a>
         <div className="ml-auto mr-4">
           {isMenuOpen ? (
             <IoCloseOutline
-              className="h-6 w-6 cursor-pointer md:hidden absolute top-0 right-0 m-2 z-10 hover:text-red-500 hover:scale-125"
+              className="text-white h-6 w-6 cursor-pointer md:hidden absolute top-0 right-0 m-2 z-10 hover:text-red-500 hover:scale-125"
               onClick={toggleMenu}
             />
           ) : (
             <GiHamburgerMenu
-              className="h-6 w-6 cursor-pointer md:hidden"
+              className="fill-white h-6 w-6 cursor-pointer md:hidden"
               onClick={toggleMenu}
             />
           )}
         </div>
         <div
           className={`${
-            isMenuOpen ? "block absolute top-0 right-0 m-2 bg-white cursor-pointer" : "hidden"
+            isMenuOpen ? "block absolute top-0 right-0 m-2  cursor-pointer" : "hidden"
           } md:flex md:items-center md:w-auto cursor-pointer`}
         >
           <div className="text-black  md:flex-grow">
             <a
               href="/"
              
-              className="block py-2 px-4 hover:scale-105 hover:text-black text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
             >
               Home
             </a>
             <a
               href="/AboutMe"
-              className="block py-2 px-4 hover:scale-105 hover:text-black text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
             >
               About
             </a>
             <a
               href="/WebProjects"
-              className="block py-2 px-4 hover:scale-105 hover:text-black text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
             >
               Web Development
             </a>
             <a
               href="/GraphicsProjects"
-              className="block py-2 px-4 hover:scale-105 hover:text-black text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
             >
               Graphics Design
+            </a>
+            <a
+              href="/GraphicsProjects"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+            >
+              Machine Learning
+            </a>
+            <a
+              href="/GraphicsProjects"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+            >
+              UI/UX Design
             </a>
             <Link
               href="#contact"
@@ -77,7 +89,7 @@ function NavBar() {
               smooth={true}
               offset={-70}
               duration={800}
-              className="block py-2 px-4 hover:scale-105 hover:text-black text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
+              className="block py-2 px-4 hover:scale-105 hover:text-white text-gray-400 rounded md:inline-block md:mt-0 md:ml-4"
             >
               Contact
             </Link>
